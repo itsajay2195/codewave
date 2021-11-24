02eb71f34602fd67a5ca91be0e973e7e9184f57e
ajayhf <ajaykumar.r@happyfox.com>
2021-11-24 16:36:53 +0530

    structuring project and adding navigation

---
+++ navigation.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './src/screens/Home'
+import Details from './src/screens/Details'
+
+
+
+
+export default function RootNavigation() {
+    const Stack = createStackNavigator();
+
+    const screenOptions = {
+        headerShown: false
+    }
+
+    return (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+                    <Stack.Screen name='Home' component={Home}></Stack.Screen>
+                    <Stack.Screen name='Details Details' component={Details}></Stack.Screen>
+                </Stack.Navigator>
+            </NavigationContainer>
+      
+    )
+}
